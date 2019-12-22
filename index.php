817c70acf13398debef46b3de9b96b0b56542317
Naxi <michal.vasut.st@vsb.cz>
2019-12-22 21:57:18 +0100

    feat: basic design
    css hell

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>MyPage</title>
+
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+
+  <div class="wrapper-page">
+
+    <div class="left-menu">
+      s<br>
+      s<br>
+      s<br>
+      s<br>
+      s<br>
+      s<br>
+      s<br>
+      s<br>
+      s<br>
+      s<br>
+      s<br>
+      s<br>
+      s<br>
+      s<br>
+      s<br>
+      s<br>
+
+    </div>
+
+    <div class="main">
+      s
+    </div>
+
+  </div>
+
+  </body>
+</html>
