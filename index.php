8b96aa2bfd56a46d0393fb24bdd14340d9498bb0
Naxi <michal.vasut.st@vsb.cz>
2019-12-29 16:40:36 +0100

    feat: menu inserted to separated file

--- index.php
+++ index.php
@@ -11,54 +11,9 @@
 
   <div class="wrapper-page">
 
-    <div class="left-menu">
-      <center><h1>Menu</h1></center>
-
-      <?php
-        $servername = "localhost";
-        $username = "Naxi";
-        $password = "tajneheslo";
-        $dbname = "myshop";
-        $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-
-        mysqli_query($conn, "SET CHARACTER SET utf8");
-
-        $sql = "SELECT type FROM guns GROUP BY type";
-        $result = mysqli_query($conn, $sql);
-
-
-        if (mysqli_num_rows($result) > 0)
-        {
-          while($row = mysqli_fetch_assoc($result))
-          {
-            $sql = "SELECT manufacture FROM guns WHERE type=\"".$row["type"]."\" GROUP BY manufacture";
-            $result_inner = mysqli_query($conn, $sql);
-
-            if (mysqli_num_rows($result_inner) > 1)
-            {
-              echo "<a href=\"#neco\"><div class=\"menu-dropdown\">".$row["type"];
-              echo "<div class=\"dropdown-content\">";
-              while($row_inner = mysqli_fetch_assoc($result_inner))
-              {
-                echo "<a href=\"#neco\"><div class=\"menu-item\">".$row_inner["manufacture"]."</div></a>";
-              }
-              echo "</div>";
-              echo "</div></a>";
-            }
-            else
-            {
-              echo "<a href=\"#neco\"><div class=\"menu-item\">".$row["type"]."</div></a>";
-            }
-
-          }
-        }
-
-      ?>
-    </div>
+    <?php
+    include("menu.php");
+    ?>
 
     <div class="main">
       <div class="products-wrapper">
